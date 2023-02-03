e7477fe779d7ee06b953316a410844081d1dade4
damilola oyeyipo <oyeyipo45@gmail.com>
2023-02-03 11:41:11 +0100

    Set up server

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import express, { Application } from 'express';
+
+const app: Application = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(Number(port), () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
